65e96345bf3c783f5b9100e10897d38c42c0dad0
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-09-03 09:43:34 +0200

    init project

---
+++ src/main/java/dev/mazurkiewicz/m2flashcards/M2flashcardsApplication.java
@@ -0,0 +1,13 @@
+package dev.mazurkiewicz.m2flashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2flashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2flashcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mazurkiewicz/m2flashcards/M2flashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mazurkiewicz.m2flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2flashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
