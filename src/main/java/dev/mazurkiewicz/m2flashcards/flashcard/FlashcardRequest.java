129c20b6a051520444872fee817a9b2524ceda17
Marcin Mazurkiewicz <marcin@mazurkiewicz.sh>
2020-09-23 10:40:48 +0200

    Validation request data for put and patch endpoints in FlashcardController

--- src/main/java/dev/mazurkiewicz/m2flashcards/flashcard/FlashcardRequest.java
+++ src/main/java/dev/mazurkiewicz/m2flashcards/flashcard/FlashcardRequest.java
@@ -3,11 +3,15 @@ package dev.mazurkiewicz.m2flashcards.flashcard;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FlashcardRequest {
 
+    @NotEmpty
     private String question;
+    @NotEmpty
     private String answer;
     private boolean twoSided;
     private boolean privy;
