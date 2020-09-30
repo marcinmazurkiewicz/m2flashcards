8efd646c132d57ebe892c71dc43a23675fc05c65
Marcin Mazurkiewicz <marcin@mazurkiewicz.dev>
2020-09-30 15:02:37 +0200

    Change UniqueMail NotNull constraint to NotBlank

--- src/main/java/dev/mazurkiewicz/m2flashcards/user/validation/UniqueMail.java
+++ src/main/java/dev/mazurkiewicz/m2flashcards/user/validation/UniqueMail.java
@@ -3,13 +3,14 @@ package dev.mazurkiewicz.m2flashcards.user.validation;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@NotEmpty
+@NotBlank
 @Email
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
